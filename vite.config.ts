024312f4c0818234f58a3b977da3202d688761a7
s-r-x <strax1990@gmail.com>
2025-09-08 16:44:33 +0500

    setup pwa / add icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   base: "/final-spritesheet",
@@ -11,6 +12,41 @@ export default defineConfig({
     tanstackRouter({
       target: "react",
       autoCodeSplitting: false,
+    }),
+    VitePWA({
+      manifest: {
+        name: "Final spritesheet",
+        short_name: "A spritesheet generator",
+        description: "A spritesheet generator",
+        theme_color: "#f2e9e4ff",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: false,
+      },
     }),
   ],
 });
