50b1db44cc96364f5601b695662f5355e72d301a
s-r-x <strax1990@gmail.com>
2025-09-11 20:51:56 +0500

    check if element global position is not NaN in canvas' focus element function before focusing one
    NaN can cause some major issues with the viewport library

--- src/canvas/focus-element.ts
+++ src/canvas/focus-element.ts
@@ -12,7 +12,11 @@ export const focusElement = ({
 }) => {
   const el = app.stage.getChildByLabel(name, true);
   if (!el) return;
-  const pos = viewport.toLocal(el.getGlobalPosition());
+  const elGlobalPos = el.getGlobalPosition();
+  if (isNaN(elGlobalPos.x) || isNaN(elGlobalPos.y)) {
+    return;
+  }
+  const pos = viewport.toLocal(elGlobalPos);
   viewport.moveCenter(pos.x + el.width / 2, pos.y + el.height / 2);
   viewport.fit(true, el.width, el.height);
 };
